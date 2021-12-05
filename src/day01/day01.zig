7e01c99a5e9f9fa8b0d8595bba68ba6d2cf37787
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-12-05 05:39:31 +0200

    Solution for Day 1 part with BoundedArray

--- src/day01/day01.zig
+++ src/day01/day01.zig
@@ -3,12 +3,14 @@ const std = @import("std");
 const testing = std.testing;
 const print = std.debug.print;
 const Allocator = std.mem.Allocator;
+const BoundedArray = std.BoundedArray;
 
 const input = @embedFile("./input.txt");
 
 pub fn main() anyerror!void {
     print("Day 1: Sonar sweep!\n", .{});
-    print("Result: {d}\n", .{part1()});
+    print("Part 1: {d}\n", .{part1()});
+    print("Part 2: {d}\n", .{part2()});
 }
 
 ///
@@ -31,4 +33,34 @@ fn part1() !i32 {
 test "day01.part1" {
     @setEvalBranchQuota(200_000);
     try testing.expectEqual(1446, comptime try part1());
+}
+
+///
+/// --- Part Two ---
+///
+fn part2() !i32 {
+    const window_length: usize = 3;
+    var increases: i32 = 0;
+    var buffer = try BoundedArray(i32, window_length + 1).init(0);
+    var lines = std.mem.split(u8, std.mem.trimRight(u8, input, "\n"), "\n");
+
+    while (lines.next()) |line| {
+        var depth = try std.fmt.parseInt(i32, line, 10);
+        try buffer.append(depth);
+
+        if (buffer.len == window_length) {
+            // As the sum of middle items are always the same, only the edges need to be compared
+            if (depth > buffer.get(0)) {
+                increases += 1;
+            }
+            _ = buffer.orderedRemove(0);
+        }
+    }
+
+    return increases;
+}
+
+test "day01.part2" {
+    @setEvalBranchQuota(200_000);
+    try testing.expectEqual(1420, comptime try part2());
 }
