e6b66e4063482d3d579fb56d0e403651c3c471df
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-12-07 00:22:10 +0200

    Day 3: Remove things that I just can't get to work. Maybe I do not understand how pointers, slices, arrays and comptime work together yet.

--- src/day03/day03.zig
+++ src/day03/day03.zig
@@ -16,50 +16,12 @@ pub fn main() anyerror!void {
     // print("Part 2: {d}\n", .{part2()});
 }
 
-fn getMask(bitWidth: u64) ![]u64 {
-    var mask = ArrayList(u64).init(std.heap.page_allocator);
-    defer mask.deinit();
-    var offset: u64 = 0;
-
-    while (offset < bitWidth) {
-        var power = math.pow(u64, 2, offset);
-        try mask.append(power);
-        offset += 1;
-    }
-
-    return mask.toOwnedSlice();
-}
-
-test "getMask" {
-    const expected = [12]u64{
-        0b0000_0000_0001,
-        0b0000_0000_0010,
-        0b0000_0000_0100,
-        0b0000_0000_1000,
-        0b0000_0001_0000,
-        0b0000_0010_0000,
-        0b0000_0100_0000,
-        0b0000_1000_0000,
-        0b0001_0000_0000,
-        0b0010_0000_0000,
-        0b0100_0000_0000,
-        0b1000_0000_0000,
-    };
-    try testing.expectEqual(expected, comptime try getMask(12)[0..12]);
-}
-
-fn bitArrayToUnsignedInt(bits: []const u2) u64 {
+fn bitArrayToUnsignedInt(bits: *[]u2) u64 {
     var integer: u64 = 0;
     for (bits) |bit| {
         integer = (integer << 1) + bit;
     }
     return integer;
-}
-
-test "bitArrayToUnsignedInt" {
-    const expected: u64 = 1970;
-    const bits = [12]u2{ 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0 };
-    try testing.expectEqual(expected, bitArrayToUnsignedInt(bits));
 }
 
 fn getArraySlice(comptime T: type, value: T, n: usize) ![]T {
@@ -73,24 +35,23 @@ fn part1() !u64 {
     const report = try readReport(input);
     const numbers = try parseNumbers(report);
     const bitWidth: u64 = report[0].len;
-    var count: u64 = numbers.len;
-    var mask = try getMask(bitWidth);
+    var total: u64 = numbers.len;
     var bits = try getArraySlice(u2, 0, bitWidth);
     var ones = try getArraySlice(u64, 0, bitWidth);
+    var offset: u64 = 0;
 
-    for (numbers) |number| {
-        var bit: u64 = 0;
-
-        while (bit < bitWidth) {
-            if (number & mask[bit] != 0) {
-                ones[bit] = @as(u64, ones[bit]) + @as(u64, 1);
+    while (offset < bitWidth) {
+        var mask = math.pow(u64, 2, bitWidth - offset);
+        for (numbers) |number| {
+            if (number & mask != 0) {
+                ones[offset] = @as(u64, ones[offset]) + @as(u64, 1);
             }
-            bit += 1;
         }
+        offset += 1;
     }
 
-    for (ones) |num, i| {
-        var bit: u2 = if (2 * num >= count) 1 else 0;
+    for (ones) |count, i| {
+        var bit: u2 = if (2 * count >= total) 1 else 0;
         bits[i] = bit;
     }
 
@@ -108,7 +69,7 @@ fn part1() !u64 {
 
 test "day03.part1" {
     @setEvalBranchQuota(200_000);
-    try testing.expectEqual(@as(u64, 3985686), comptime try part1());
+    try testing.expectEqual(@as(u64, 3985686), try part1());
 }
 
 fn readReport(whole_input: []const u8) ![][]const u8 {
